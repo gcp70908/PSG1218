e3f5283ed175040206f7a900d6fe744b0446c870
gcp70908 <91703821+gcp70908@users.noreply.github.com>
2021-10-01 07:04:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
 # sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
@@ -15,5 +15,5 @@ sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_gener
 # git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 
 # 去除默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
